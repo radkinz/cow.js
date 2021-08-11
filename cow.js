a6aa266c15121e9f213410359db7e58abfc3d04e
Ruby Adkins <radkins22@students.stab.org>
2021-08-10 22:29:48 -0400

    make cow continous

--- cow.js
+++ cow.js
@@ -2,7 +2,7 @@
 function image(width, id) {
     //dynamically add an image and set its attribute
     var img = document.createElement("img");
-    img.src = "images/cow.gif";
+    img.src = "/assets/cow.gif";
     img.width = width;
     var parent = document.getElementById(id);
     parent.appendChild(img);
@@ -10,7 +10,9 @@ function image(width, id) {
     //position img
     img.style.position = 'absolute';
     img.style.left = ((window.innerWidth - img.width)) + "px";
-    img.style.top = 100 + "px";
+
+    //100 as a buffer
+    img.style.top = random(0, (parent.clientHeight-100)) + "px";
 
     return img
 }
@@ -31,12 +33,6 @@ function sound(src) {
     }
 }
 
-function random(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
-}
-
 var Cow = function (options) {
     //add div css for cow image
     document.getElementById(options.id).style.position = "absolute";
@@ -45,6 +41,7 @@ var Cow = function (options) {
     document.getElementById(options.id).style.right = 0;
     document.getElementById(options.id).style.bottom = 0;
     document.getElementById(options.id).style.zIndex = 1000;
+    document.getElementById(options.id).style.overflow = "hidden";
 
     //set width
     this.width = 100;
@@ -54,7 +51,7 @@ var Cow = function (options) {
     this.xpos = this.img.x;
 
     //start moo sound of cow
-    this.moo = new sound("/images/moo.mp3")
+    this.moo = new sound("/assets/moo.mp3")
     this.moo.play();
 
     //move function
@@ -63,9 +60,18 @@ var Cow = function (options) {
         this.xpos -= 2;
         this.img.style.left = this.xpos + "px";
 
-        //turn sound off if go offscreen
+        //if go offscreen move back up front with some buffer
         if (this.xpos < -this.width) {
-            this.moo.stop();
+            this.xpos = window.innerWidth;
+            
+            //randomize y pos
+            this.img.style.top = random(0, (document.getElementById(options.id).clientHeight-100)) + "px"; 
         }
     }
+}
+
+function random(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 }
