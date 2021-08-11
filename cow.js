0e1892eb3142580004e1af4b170f0931151732e7
Ruby Adkins <radkins22@students.stab.org>
2021-08-10 22:53:02 -0400

    add export line

--- cow.js
+++ cow.js
@@ -75,3 +75,5 @@ function random(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 }
+
+export default Cow;
